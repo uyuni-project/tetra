384ad6ac30b0d04bf89b29c8a57fa027d12be8b8
Silvio Moioli <silvio@moioli.net>
2013-05-20 17:15:14 +0200

    Common error conditions handled

--- lib/gjp/cli.rb
+++ lib/gjp/cli.rb
@@ -7,7 +7,15 @@ class MainCommand < Clamp::Command
     parameter "[PATH]", "project directory or jar file path", :default => "."
 
     def execute
-      puts PomGetter.get_pom(path)
+      begin
+        puts PomGetter.get_pom(path)
+      rescue Zip::ZipError
+        $stderr.puts "#{path} does not seem to be a valid jar archive, skipping"
+      rescue TypeError
+        $stderr.puts "#{path} seems to be a valid jar archive but is corrupt, skipping"
+      rescue RestClient::ResourceNotFound
+        $stderr.puts "Got an error while looking for #{path} in search.maven.org" 
+      end
     end
   end
 end
