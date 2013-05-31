73af1aa1157e12cf4e52557ed53cd42954eaf494
Silvio Moioli <smoioli@suse.de>
2013-05-31 14:02:46 +0200

    SVN checkout added to get-source

--- lib/gjp/get_source.rb
+++ lib/gjp/get_source.rb
@@ -10,20 +10,21 @@ class SourceGetter
     $log.info("downloading: #{address} in #{directory}, pomfile: #{pomfile}")
     
     dummy, prefix, scm_address = address.split(/^([^:]+):(.*)$/)
-    
     $log.info("prefix: #{prefix}, scm_address: #{scm_address}")
-        
-    if prefix == "git"
-      get_source_from_git(scm_address, pomfile, directory)
-    end
+	
+		get_source_from_scm(prefix, scm_address, pomfile, directory)
   end
 
-  # checks out from git
-  def self.get_source_from_git(scm_address, pomfile, directory)
+  # checks out code from git, svn...
+  def self.get_source_from_scm(prefix, scm_address, pomfile, directory)
     pom = Pom.new(pomfile)
     dir = File.join(directory, "#{pom.group_id}:#{pom.artifact_id}:#{pom.version}")
-    
     Dir::mkdir(dir)
-    `git clone #{scm_address} #{dir}`
+    
+		if prefix == "git"
+			`git clone #{scm_address} #{dir}`
+    elsif prefix == "svn"
+			`svn checkout #{scm_address} #{dir}`
+		end
   end
 end
--- spec/lib/get_source_spec.rb
+++ spec/lib/get_source_spec.rb
@@ -19,5 +19,21 @@ describe SourceGetter do
       FileUtils.rm_rf(repo_path)
     end
   end
+  
+	describe ".get_source_from_svn" do
+    it "gets the sources from an svn repo" do
+      
+      dir_path = File.join("spec", "data", "struts-apps")
+      pom_path = File.join(dir_path, "pom.xml")
+
+      SourceGetter.get_source("svn:http://svn.apache.org/repos/asf/struts/struts2/tags/STRUTS_2_3_14/apps", pom_path, dir_path)
+
+      repo_path = File.join(dir_path, "org.apache.struts:struts2-apps:", "showcase")
+      file_path = File.join(repo_path, "README.txt")
+      File.open(file_path).readline.should eq "README.txt - showcase\n"
+      
+      FileUtils.rm_rf(repo_path)
+    end
+  end
 end
 
