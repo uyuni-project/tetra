031866955cd1a58c4ed76552d0c5a0b62c2b73ec
Silvio Moioli <smoioli@suse.de>
2013-08-08 08:16:04 +0200

    gjp.rb file reorganization

--- lib/gjp.rb
+++ lib/gjp.rb
@@ -1,13 +1,17 @@
-require "gjp/logger"
+# essential files
 require "gjp/version"
+
+# core classes
 require "gjp/logger"
 require "gjp/cli"
 require "gjp/pom"
 require "gjp/project"
 require "gjp/version_matcher"
 require "gjp/maven_website"
+require "gjp/jar_table"
+
+# subcommand implementation classes
 require "gjp/get_pom"
 require "gjp/get_parent_pom"
 require "gjp/get_source_address"
 require "gjp/get_source"
-require "gjp/jar_table"
