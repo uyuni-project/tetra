10ce0a6a5bcdc9b413728cc6ed8272dc28772e5e
Silvio Moioli <smoioli@suse.de>
2013-08-08 08:43:02 +0200

    Refactoring: file reorganization

--- lib/gjp.rb
+++ lib/gjp.rb
@@ -1,9 +1,5 @@
-# essential files
 require "gjp/version"
-
-# core classes
 require "gjp/logger"
-require "gjp/cli"
 require "gjp/pom"
 require "gjp/project"
 require "gjp/version_matcher"
@@ -11,10 +7,6 @@ require "gjp/maven_website"
 require "gjp/jar_table"
 require "gjp/limited_network_user"
 
-# subcommand implementation classes
-require "gjp/get_pom"
-require "gjp/get_parent_pom"
-require "gjp/get_source_address"
-require "gjp/get_source"
-require "gjp/set_up_nonet_user"
-require "gjp/tear_down_nonet_user"
+require "gjp/cli"
+
+Dir[File.dirname(__FILE__) + '/gjp/subcommands/*.rb'].each { |file| require file }
