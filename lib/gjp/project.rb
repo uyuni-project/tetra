a69b69399a348f22615166bc1d5185c78b09fe84
Silvio Moioli <smoioli@suse.de>
2013-09-13 08:47:26 +0200

    Bugfix: do not generate file lists for . and ..

--- lib/gjp/project.rb
+++ lib/gjp/project.rb
@@ -133,7 +133,7 @@ module Gjp
     # package (subdirectory) in src/
     def update_changed_src_file_list(list_name, tag)
       Dir.foreach("src") do |entry|
-        if File.directory?(File.join(Dir.getwd, "src", entry))
+        if File.directory?(File.join(Dir.getwd, "src", entry)) and entry != "." and entry != ".."
           update_changed_file_list(File.join("src", entry), "#{entry}_#{list_name.to_s}", tag)
         end
       end
