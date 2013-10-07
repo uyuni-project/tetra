abe10a175fe1e36e70380d4b89923156832213e5
Silvio Moioli <smoioli@suse.de>
2013-10-07 15:17:50 +0200

    Refactoring: Nokogiri expression semplifications

--- lib/gjp/pom.rb
+++ lib/gjp/pom.rb
@@ -19,45 +19,45 @@ module Gjp
     end
     
     def group_id
-      @doc.xpath("project/groupId/text()").to_s
+      @doc.xpath("project/groupId").text
     end
     
     def artifact_id
-      @doc.xpath("project/artifactId/text()").to_s
+      @doc.xpath("project/artifactId").text
     end
     
     def version
-      @doc.xpath("project/version/text()").to_s
+      @doc.xpath("project/version").text
     end
 
     def description
-      @doc.xpath("project/description/text()").to_s
+      @doc.xpath("project/description").text
     end
 
     def url
-      @doc.xpath("project/url/text()").to_s
+      @doc.xpath("project/url").text
     end
 
     def license_name
-      @doc.xpath("project/licenses/license/name/text()").to_s
+      @doc.xpath("project/licenses/license/name").text
     end
 
     def runtime_dependency_ids
       result = @doc.xpath("project/dependencies/dependency[not(optional='true') and not(scope='provided') and not(scope='test') and not(scope='system')]").map do |element|
-        [element.xpath("groupId/text()").to_s, element.xpath("artifactId/text()").to_s, element.xpath("version/text()").to_s]
+        [element.xpath("groupId").text, element.xpath("artifactId").text, element.xpath("version").text]
       end
     end
 
     def parent_group_id
-      @doc.xpath("project/parent/groupId/text()").to_s
+      @doc.xpath("project/parent/groupId").text
     end
     
     def parent_artifact_id
-      @doc.xpath("project/parent/artifactId/text()").to_s
+      @doc.xpath("project/parent/artifactId").text
     end
     
     def parent_version
-      @doc.xpath("project/parent/version/text()").to_s
+      @doc.xpath("project/parent/version").text
     end
   end
 end
