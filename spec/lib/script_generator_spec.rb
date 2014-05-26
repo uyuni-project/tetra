57e02802f23f3b8df3a173a31ec8480a749f6513
Silvio Moioli <smoioli@suse.de>
2014-05-26 16:32:33 +0200

    Refactoring: fixing rubocop LineEndConcatenation offenses

--- spec/lib/script_generator_spec.rb
+++ spec/lib/script_generator_spec.rb
@@ -41,7 +41,7 @@ describe Gjp::ScriptGenerator do
 
         lines.should include("#!/bin/bash\n")
         lines.should include("cd somewhere significant\n")
-        lines.should include("$PROJECT_PREFIX/kit/mvn/bin/mvn -Dmaven.repo.local=$PROJECT_PREFIX/kit/m2 " +
+        lines.should include("$PROJECT_PREFIX/kit/mvn/bin/mvn -Dmaven.repo.local=$PROJECT_PREFIX/kit/m2 " \
           "-s$PROJECT_PREFIX/kit/m2/settings.xml -o --options\n"
         )
 
