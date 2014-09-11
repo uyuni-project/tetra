c564b382680ef8c46f9548579c6c47779d6c4426
Silvio Moioli <silvio@moioli.net>
2014-09-11 13:25:47 +0100

    Enable glue kit item spec generation

--- lib/tetra/glue_kit_item.rb
+++ lib/tetra/glue_kit_item.rb
@@ -4,12 +4,26 @@ module Tetra
   # represents a prebuilt package dependency from a jar file
   # in a kit
   class GlueKitItem
+    # implement to_spec
+    include SpecGenerator
+    attr_reader :project
+    attr_reader :package_name
+    attr_reader :spec_dir
+    attr_reader :template_spec_name
+
     attr_reader :provides_symbol
     attr_reader :provides_version
+    attr_reader :install_dir
 
     def initialize(project)
+      @project = project
+      @package_name = "kit-glue-#{project.name}"
+      @spec_dir = "kit"
+      @template_spec_name = "kit_item.spec"
+
       @provides_symbol = "kit-glue(#{project.name})"
       @provides_version = project.version
+      @install_dir = ""
     end
   end
 end
