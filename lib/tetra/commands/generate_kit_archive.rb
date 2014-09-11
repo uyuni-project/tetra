fb8d44630e6f6a01c60eeb0b158c47da93d6965a
Silvio Moioli <silvio@moioli.net>
2014-09-11 11:38:18 +0100

    Enable Maven kit item archive generation

--- lib/tetra/commands/generate_kit_archive.rb
+++ lib/tetra/commands/generate_kit_archive.rb
@@ -7,8 +7,15 @@ module Tetra
       checking_exceptions do
         project = Tetra::Project.new(".")
         ensure_dry_running(false, project) do
-          result_path = Tetra::Kit.new(project).to_archive
+          kit = Tetra::Kit.new(project)
+
+          result_path = kit.to_archive
           print_generation_result(project, result_path)
+
+          kit.maven_kit_items.each do |item|
+            result_path = item.to_archive
+            print_generation_result(project, result_path)
+          end
         end
       end
     end
