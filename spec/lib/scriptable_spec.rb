c84dd439e7906fdc4fa97f0e9c345a92954566dd
Silvio Moioli <smoioli@suse.de>
2015-02-03 15:01:06 +0100

    Bugfix: use correct mvn path in test

--- spec/lib/scriptable_spec.rb
+++ spec/lib/scriptable_spec.rb
@@ -40,7 +40,7 @@ describe Tetra::Scriptable do
 
         expect(lines).to include("#!/bin/bash\n")
         expect(lines).to include("cd somewhere significant\n")
-        expect(lines).to include("$PROJECT_PREFIX/kit/apache-maven-3.2.5/bin/mvn \
+        expect(lines).to include("$PROJECT_PREFIX/kit/mvn/bin/mvn \
 -Dmaven.repo.local=$PROJECT_PREFIX/kit/m2 --settings $PROJECT_PREFIX/kit/m2/settings.xml \
 --strict-checksums -o --options\n"
         )
