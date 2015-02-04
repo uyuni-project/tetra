5a683515bf7b38053b67491258e86ebe320ac7ce
Silvio Moioli <smoioli@suse.de>
2015-02-04 15:41:57 +0100

    Refactoring: use functions in DryRunSubcommand

--- lib/tetra/ui/dry_run_subcommand.rb
+++ lib/tetra/ui/dry_run_subcommand.rb
@@ -14,31 +14,38 @@ module Tetra
     def execute
       checking_exceptions do
         project = Tetra::Project.new(".")
+        send(command, project)
+      end
+    end
 
-        if command == "start"
-          if !project.dry_running?
-            project.dry_run
-            puts "Now dry-running, please start your build."
-            puts "To run a Maven installation from the kit, use \"tetra mvn\"."
-            puts "If the build succeedes end this dry run with \"tetra dry-run finish\"."
-            puts "If the build does not succeed use \"tetra dry-run abort\" to undo any change."
-          else
-            puts "Dry-run already in progress."
-            puts "Use \"tetra dry-run finish\" to end it or \"tetra dry-run abort\" to undo changes."
-          end
-        else # finish || abort
-          if project.dry_running?
-            if command == "finish"
-              project.finish
-              puts "Dry-run finished."
-            else #abort
-              project.abort
-              puts "Project reverted as before dry-run."
-            end
-          else
-            puts "No dry-run is in progress."
-          end
-        end
+    def start(project)
+      if !project.dry_running?
+        project.dry_run
+        puts "Now dry-running, please start your build."
+        puts "To run a Maven installation from the kit, use \"tetra mvn\"."
+        puts "If the build succeedes end this dry run with \"tetra dry-run finish\"."
+        puts "If the build does not succeed use \"tetra dry-run abort\" to undo any change."
+      else
+        puts "Dry-run already in progress."
+        puts "Use \"tetra dry-run finish\" to end it or \"tetra dry-run abort\" to undo changes."
+      end
+    end
+
+    def finish(project)
+      if project.dry_running?
+        project.finish
+        puts "Dry-run finished."
+      else
+        puts "No dry-run is in progress."
+      end
+    end
+
+    def abort(project)
+      if project.dry_running?
+        project.abort
+        puts "Project reverted as before dry-run."
+      else
+        puts "No dry-run is in progress."
       end
     end
   end
