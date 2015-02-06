ebf0f1e35e733096db812f4b6397352195d1b8eb
Silvio Moioli <smoioli@suse.de>
2015-02-06 10:23:28 +0100

    Refactoring: remove useless integration test part

--- integration-tests/build-commons.sh
+++ integration-tests/build-commons.sh
@@ -20,15 +20,6 @@ tetra dry-run finish
 tetra generate-kit
 tetra generate-archive
 tetra generate-spec
-# simulate tetra generate-script
-cd ../..
-cat >packages/commons-collections/build.sh <<"EOF"
-#!/bin/bash
-PROJECT_PREFIX=`readlink -e .`
-cd .
-cd src/commons-collections-3.2.1-src/
-$PROJECT_PREFIX/kit/apache-maven-3.2.5/bin/mvn -Dmaven.repo.local=$PROJECT_PREFIX/kit/m2 -s$PROJECT_PREFIX/kit/m2/settings.xml -o package -DskipTests
-EOF
 
 echo "**************** All Done ****************"
 
