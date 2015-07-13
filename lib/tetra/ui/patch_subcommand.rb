0a7533a629fc3dee2733fcbeba845b8ee3bbd2d5
Silvio Moioli <smoioli@suse.de>
2015-07-13 15:28:34 +0200

    Bugfix: add minimal output to tetra patch

--- lib/tetra/ui/patch_subcommand.rb
+++ lib/tetra/ui/patch_subcommand.rb
@@ -10,6 +10,7 @@ module Tetra
         project = Tetra::Project.new(".")
         ensure_dry_running(:is_not_in_progress, project) do
           project.commit_sources(message, false)
+          puts "Patch recorded, use \"tetra generate-spec\" to update the specfile"
         end
       end
     end
